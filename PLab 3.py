e3d5a9e4e4dc5822ec7afc12b575c70d45ab9e67
krakesh80888-design <krakesh80888@gmail.com>
2026-04-30 08:10:56 +0530

    Add files via upload

---
+++ PLab 3.py
@@ -0,0 +1,8 @@
+#Write a Python program to display the multiplication table of a given number.
+num = int(input("Enter a number: "))
+
+# Display multiplication table
+print(f"\nMultiplication Table of {num}:")
+
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
